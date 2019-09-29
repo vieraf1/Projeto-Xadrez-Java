96dc9a319457dabd37a61b9e35de495a865b8a8a
Lucas Vieira <vieraf1@hotmail.com.br>
2019-09-29 13:59:36 -0300

    Movendo Peças

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,8 @@
 package application;
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
 import chess.ChessPosition;
 
 public class Program {
@@ -17,15 +20,29 @@ public class Program {
 1 - - - - - - - - 
   a b c d e f g h
 		 */
-		
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+		
+		while(true) {
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.print("Souce: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target) ;
+		}
 		
 		//ChessPosition chessPosition = new ChessPosition('a',1);
 		//chessPosition.toPosition();
 		
 		//System.out.println("coluna " + (char) ('a' + 0));
 		//System.out.println("linha " + (8 - 6));
+		
+		//sc.close();
 	}
 
 }

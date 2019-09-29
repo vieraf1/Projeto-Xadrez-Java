8e76ece3050b6c28d83804433e77ea35b66d7b1b
Lucas Vieira <vieraf1@hotmail.com.br>
2019-09-29 12:44:38 -0300

    ChessPosition e ChessException

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,31 @@
 package application;
 import chess.ChessMatch;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+		/*
+8 - - - - - - - - 
+7 - - - - - - - - 
+6 - - - - - - - - 
+5 - - - - - - - - 
+4 - - - - - - - - 
+3 - - - - - - - - 
+2 - - - - - - - - 
+1 - - - - - - - - 
+  a b c d e f g h
+		 */
+		
+		//ChessMatch chessMatch = new ChessMatch();
+		//UI.printBoard(chessMatch.getPieces());
+		
+		//ChessPosition chessPosition = new ChessPosition('a',1);
+		//chessPosition.toPosition();
+		
+		//System.out.println("coluna " + (char) ('a' + 0));
+		//System.out.println("linha " + (8 - 6));
 	}
 
 }

cdb733ca8833a1c0d328c2d99293839b212a817a
Lucas Vieira <vieraf1@hotmail.com.br>
2019-10-05 14:20:19 -0300

    Imprimindo posições possiveis de movimento

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,6 +33,10 @@ public class Program {
 				System.out.println();
 				System.out.print("Souce: ");
 				ChessPosition source = UI.readChessPosition(sc);
+				
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 
 				System.out.println();
 				System.out.print("Target: ");
